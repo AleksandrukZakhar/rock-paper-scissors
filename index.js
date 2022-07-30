222bc22ba0ab5e4000f4cf36f9d036ac390d47a6
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-07-30 10:42:12 +0300

    Add playRound function

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const items = ["rock", "paper", "scissors"];
-const playerChoice = prompt("Choose your weapon");
+const playerChoice = prompt("Choose your weapon").toLowerCase();
 
 const computerChoice = () => items[Math.floor(Math.random() * 3)];
+
+const playRound = () => {
+  const current_case = playerChoice + " " + computerChoice();
+
+  const cases = ["rock scissors", "scissors paper", "paper rock"];
+
+  if (
+    current_case === cases[0] ||
+    current_case === cases[1] ||
+    current_case === cases[2]
+  ) {
+    alert("You won");
+    return;
+  }
+
+  alert("You lose");
+  return;
+};
+
+playRound();
