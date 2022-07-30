4135696cec271fa5ab6cf3d7ee64120f9b10e743
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-07-30 10:48:26 +0300

    Add playGame function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const items = ["rock", "paper", "scissors"];
-const playerChoice = prompt("Choose your weapon").toLowerCase();
-
+const playerChoice = () => prompt("Choose your weapon").toLowerCase();
 const computerChoice = () => items[Math.floor(Math.random() * 3)];
+let playerScore = 0;
+let computerScore = 0;
 
 const playRound = () => {
-  const current_case = playerChoice + " " + computerChoice();
+  const current_case = playerChoice() + " " + computerChoice();
 
   const cases = ["rock scissors", "scissors paper", "paper rock"];
 
@@ -13,12 +14,24 @@ const playRound = () => {
     current_case === cases[1] ||
     current_case === cases[2]
   ) {
+    playerScore += 1;
     alert("You won");
     return;
   }
 
   alert("You lose");
+  computerScore += 1;
   return;
 };
 
-playRound();
+const playGame = () => {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
+
+  playerScore > computerScore
+    ? alert(`You won with the score of ${playerScore}`)
+    : alert(`Computer won with the score of ${computerScore}`);
+};
+
+playGame();
