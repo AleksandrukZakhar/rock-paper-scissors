debc460c73afcc1be449b80eb64c437bc453c98f
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-08-03 13:24:55 +0300

    js logic

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-const items = ["rock", "paper", "scissors"];
-const getPlayerChoice = () => prompt("Choose your weapon").toLowerCase();
-const getComputerChoice = () => items[Math.floor(Math.random() * 3)];
+const items = ["👊", "✋", "✌️"];
+const btn = document.querySelectorAll(".btn");
+const playerWeapon = document.querySelector(".player-weapon");
+const computerWeapon = document.querySelector(".computer-weapon");
+let currentPlayerChoice = null;
+let currentComputerChoice = null;
 let playerScore = 0;
 let computerScore = 0;
 
-const playRound = () => {
-  const currentPlayerChoice = getPlayerChoice();
-  const currentComputerChoice = getComputerChoice();
+const getPlayerChoice = (data) => {
+  const choice = data;
+  currentPlayerChoice = choice;
+  playerWeapon.textContent = choice;
+  console.log(`Players choice ${choice}`);
+
+  getComputerChoice();
+
+  return choice;
+};
+
+const getComputerChoice = () => {
+  const choice = items[Math.floor(Math.random() * 3)];
+  currentComputerChoice = choice;
+  computerWeapon.textContent = choice;
+  console.log(`Computers choice ${choice}`);
+
+  return choice;
+};
+
+btn.forEach((button) =>
+  button.addEventListener("click", () =>
+    getPlayerChoice(button.getAttribute("weapon-data"))
+  )
+);
+
+const nullChoices = () => {
+  currentPlayerChoice = null;
+  currentComputerChoice = null;
+  playerWeapon.textContent = "❔";
+  computerWeapon.textContent = "❔";
+};
+
+const decideWin = () => {
   const current_case = currentPlayerChoice + " " + currentComputerChoice;
 
-  const cases = ["rock scissors", "scissors paper", "paper rock"];
+  const cases = ["👊 ✌️", "✌️ ✋", "✋ 👊"];
 
   if (
     current_case === cases[0] ||
@@ -17,30 +51,55 @@ const playRound = () => {
     current_case === cases[2]
   ) {
     playerScore += 1;
-    alert("You won");
+    alert(`${playerScore} ${computerScore}`);
+    nullChoices();
     return;
   }
 
   if (currentPlayerChoice === currentComputerChoice) {
     playerScore += 0.5;
     computerScore += 0.5;
-    alert("Tie");
+    alert(`${playerScore} ${computerScore}`);
+    nullChoices();
     return;
   }
 
-  alert("You lose");
   computerScore += 1;
+  alert(`${playerScore} ${computerScore}`);
+  nullChoices();
   return;
 };
 
-const playGame = () => {
-  for (let i = 0; i < 5; i++) {
-    playRound();
+const playRound = () => {
+  console.log(currentPlayerChoice !== null);
+  console.log(currentComputerChoice !== null);
+  if (!currentPlayerChoice && !currentComputerChoice) {
+    console.log(`Hello ${currentPlayerChoice}`);
+    console.log(`Hello ${currentComputerChoice}`);
+    setTimeout(() => playRound(), 100);
   }
+  console.log(currentPlayerChoice !== null);
+  console.log(currentComputerChoice !== null);
+  if (currentPlayerChoice !== null && currentComputerChoice !== null) {
+    console.log("there is player choice");
+    decideWin();
+  }
+};
 
-  playerScore > computerScore
-    ? alert(`You won with the score of ${playerScore}`)
-    : alert(`Computer won with the score of ${computerScore}`);
+const playGame = () => {
+  console.log(playerScore >= 5);
+  console.log(computerScore >= 5);
+  if (playerScore >= 5 || computerScore >= 5) {
+    alert("Someone won");
+    playerScore > computerScore
+      ? alert(`You won with the score of ${playerScore}`)
+      : alert(`Computer won with the score of ${computerScore}`);
+
+    return;
+  } else {
+    playRound();
+    playGame();
+  }
 };
 
 playGame();
